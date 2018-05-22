c20df6d13b811499d833308b12c14ecc748df197
Harpal Dhillon <harpal.dhillon@citycomsolutions.com>
2018-05-22 23:15:31 +0100

    new file added

---
+++ src/org/sidhana/Utilities.groovy
@@ -0,0 +1,9 @@
+package org.sidhana
+
+class Utilities implements Serializable {
+  def steps
+  Utilities(steps) {this.steps = steps}
+  def mvn(args) {
+    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+  }
+}
