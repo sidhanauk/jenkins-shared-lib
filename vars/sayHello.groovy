6adec1b0cfc05de25eb4b9ea1cdb5ec1cef1c9a8
sidhanauk <sidhanauk@gmail.com>
2021-02-02 12:37:28 +0000

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,7 +3,9 @@ import org.sidhana.Utilities
 def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    def utils = new Utilities()
+    def utils = new Utilities(this)
+    
+    utils.mvn 'clean package'
     
     //utils.mvn(" --version")
     
