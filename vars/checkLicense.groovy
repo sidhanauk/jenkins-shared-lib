93b739e84b350c2016f35fe80e9d05a633395609
sidhanauk <sidhanauk@gmail.com>
2021-02-02 12:59:38 +0000

    Update checkLicense.groovy

--- vars/checkLicense.groovy
+++ vars/checkLicense.groovy
@@ -1,3 +1,6 @@
-def call(){
-  echo "Check license"
+// vars/sayHello.groovy
+def call(String name = 'human') {
+    // Any valid steps can be called from this code, just like in other
+    // Scripted Pipeline
+    echo "Check License"
 }
