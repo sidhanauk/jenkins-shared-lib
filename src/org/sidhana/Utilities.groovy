d1fca4adccb48bc5ecd7bed535c3d842dff4c718
sidhanauk <sidhanauk@gmail.com>
2021-02-02 15:52:19 +0000

    Update Utilities.groovy

--- src/org/sidhana/Utilities.groovy
+++ src/org/sidhana/Utilities.groovy
@@ -1,9 +1,18 @@
 package org.sidhana
 
 class Utilities implements Serializable {
-  def steps
-  Utilities(steps) {this.steps = steps}
-  def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+  //def steps
+  //Utilities(steps) {this.steps = steps}
+  //def mvn(args) {
+  //  steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+ //}
+  
+  checkJava(){
+    echo "Java"
+  }
+  
+  
+  checkPython(){
+    echo "Python"
   }
 }
